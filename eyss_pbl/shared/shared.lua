4a2e729f5f8e7e3275f6d9a8c8fd26654ec79ca1
Eyss <76251650+EyssDev@users.noreply.github.com>
2022-12-26 23:20:00 +0100

    Add files via upload

---
+++ eyss_pbl/shared/shared.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config = {
+
+    Cooldown = 15, -- Temps en seconde avant de pouvoir refouiller une poubelle
+    Couleur = "p", -- p = Purple 
+    Props = {
+        {model = 1437508529},
+        {model = -206690185}
+    },
+
+    Item_List = {
+
+        {content = "canettevide", number = 1, id = 1},
+        {content = "bread", number = 1, id = 3},
+        {content = "water", number = 2, id = 4}
+    },
+}
+
